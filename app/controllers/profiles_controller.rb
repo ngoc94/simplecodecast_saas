baa5ccd742ee787a869dc49ca70f288b8d9ad885
Ngoc Nguyen <ngoc94@gmail.com>
2016-01-07 16:56:08 +0000

    Updated files

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # Form for each User profile.
+        @User = User.find(params[:user_id])
+        @variable = params[:hello]
+        @profile = @user.build_profile        
+    end
 end
