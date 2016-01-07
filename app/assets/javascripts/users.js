99f4d7357481f66dae47536d68f19ddf9ec17111
Ngoc Nguyen <ngoc94@gmail.com>
2016-01-07 15:28:00 +0000

    Updated routes file with new devise

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,38 +1,33 @@
-$(document).ready(function() {
-    Stripe.setPublishableKey($('meta[name="Stripe-key"]').attr('content'));
-    // Watch for a form submission
+/*global Stripe*/
+
+$(document).ready(function(){
+    Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+    //watch for a form submission
     $("#form-submit-btn").click(function(event){
-        event.preventDefault();
-        $('input[type=submit]').prop('disabled', true);
-        var error    = false;
-        var ccNum    = $('#card_number').val(),
-            cvcNum   = $('#card_code').val(),
+        event.preventDefault(); //stop the button from sending form to server
+        $('input[type=submit]').prop('disabled', true); //disables the button
+        var error = false; //error noti.
+        var ccNum = $('#card_number').val(), //just value store variables
+            cvcNum = $('#card_code').val(),
             expMonth = $('#card_month').val(),
-            expYear  = $('#card_year').val();
-            
-        if (!error) {
-            // Get the Stripe token:
+            expYear = $('card_year').val();
+
+        if (!error){
+            //Get the stripe token:
             Stripe.createToken({
-                number: ccNum,
+                number:ccNum,
                 cvc: cvcNum,
                 exp_month: expMonth,
-                exp_year: expYear
+                exp_year: expYear,
             }, stripeResponseHandler);
         }
         return false;
-     }); // Form Submission
-      
-      function stripeResponseHandler(status, response){
-          // Get a reference to the form:
-          var f = $("#new_user");
-          
-          // Get the token from the response:
-          var token = response.id;
-          
-          // Add the token to the form:
-          f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '"/>');
-          
-          // Submit the form:
-          f.get(0).submit();      
+    });
+
+    function stripeResponseHandler(status, response){
+        var f = $('#new_user');
+        var token = response.id;
+        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+        f.get(0).submit(); //submission of form
     }
 });
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,3 @@
+require "stripe"
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
