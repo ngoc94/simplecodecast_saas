197a3177c116ec28debef4ed0339e222bcfc8dcf
Ngoc Nguyen <ngoc94@gmail.com>
2016-01-07 17:03:57 +0000

    Modified profiles_controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
     def new
         # Form for each User profile.
-        @User = User.find(params[:user_id])
+        @user = User.find(params[:user_id])
         @variable = params[:hello]
         @profile = @user.build_profile        
     end
